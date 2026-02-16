fab851d15f35c3c3ac0f5e4662866630269afe8e
Milán Németh <milan.nemeth@atos.net>
2026-02-16 15:58:19 +0000

    init frontend

---
+++ source/WebUI/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('WebUI');
+}
