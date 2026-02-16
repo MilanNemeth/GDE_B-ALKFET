ad1f7f0de4c33f126a746b4e127789b7164f5351
Milán Németh <milan.nemeth@atos.net>
2026-02-16 16:00:52 +0000

    remove mistaken import

--- source/WebUI/src/app/app.ts
+++ source/WebUI/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
